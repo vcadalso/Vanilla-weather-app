72ac4f00eae9bb7f66273e951957d9e66ca26088
vcadalso <vanessa.cadalso@gmail.com>
2023-06-21 17:52:00 -0400

    Update api

--- vanilla-weather-application/src/app.js
+++ vanilla-weather-application/src/app.js
@@ -17,7 +17,7 @@ function formatDate(timestamp) {
 function displayTemperature(response) {
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
-let description = document.querySelector("#description");
+let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector ("#humidity");
 let windElement = document.querySelector ("#wind");
 let dateElement = document.querySelector ("#date");
@@ -30,6 +30,7 @@ dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
 }
 let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+let city = "Miami";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
