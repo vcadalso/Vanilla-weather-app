3b75cece878ad7961d4546c6d7f99078b388feee
vcadalso <vanessa.cadalso@gmail.com>
2023-06-21 17:45:06 -0400

    update time

--- vanilla-weather-application/src/app.js
+++ vanilla-weather-application/src/app.js
@@ -1,16 +1,35 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = timestamp.getHours();
+    if (hours < 10) {
+        hours = '0${hours}';
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = '0${minutes}';
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[date.getDay()];
+
+    return '${day} ${hours} ${minutes}';
+}
+
 function displayTemperature(response) {
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let description = document.querySelector("#description");
 let humidityElement = document.querySelector ("#humidity");
 let windElement = document.querySelector ("#wind");
+let dateElement = document.querySelector ("#date");
 temperatureElement.innerHTML= Math.round (response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round (response.data.wind.speed);
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
 }
-let apiKey = "0300e14043fb59596a12t1bb17o183cf";
-let apiUrl = "https://api.shecodes.io/weather/v1/current?query={Miami}&key={apiKey}&units=metric";
+let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
