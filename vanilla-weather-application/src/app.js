a37d04f52219f0db44ddf669d0330d546ddc0a2c
vcadalso <vanessa.cadalso@gmail.com>
2023-06-26 17:56:55 -0400

    Implemented the unit conversion

--- vanilla-weather-application/src/app.js
+++ vanilla-weather-application/src/app.js
@@ -23,7 +23,9 @@ let windElement = document.querySelector ("#wind");
 let dateElement = document.querySelector ("#date");
 let iconElement = document.querySelector ("#icon");
 
-temperatureElement.innerHTML= Math.round (response.data.main.temp);
+celsiusTemperature = response.data.main.temp;
+
+temperatureElement.innerHTML= Math.round (celsiusTemperature);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
@@ -44,9 +46,37 @@ axios.get(apiUrl).then(displayTemperature);
 function handleSubmit(event) {
 event.preventDefault();
 let cityInputElement = document.querySelector("#city-input");
-search (cityInputElement.value);
+search(cityInputElement.value);
 }
 
+function displayFahrenheitTemperature(event){
+    event.preventDefault();
+     let temperatureElement = document.querySelector ("#temperature");
+     celsiusLink.classList.remove("active");
+     fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    alert("fahrenheitTemperature");
+   
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+event.preventDefault();
+celsiusLink.classList.add("active");
+fahrenheitLink.classList.remove("active");
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = documents.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click" , displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click" , displayCelsiusTemperature);
+
+search ("New York");
