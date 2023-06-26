fe369ca10bb77e9ea54adbd68f359ae8ea26d9f4
vcadalso <vanessa.cadalso@gmail.com>
2023-06-26 16:53:52 -0400

    Added the search engine

--- vanilla-weather-application/src/app.js
+++ vanilla-weather-application/src/app.js
@@ -35,8 +35,18 @@ iconElement.setAttribute(
   );
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
 let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
 let city = "Miami";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+event.preventDefault();
+let cityInputElement = document.querySelector("#city-input");
+search (cityInputElement.value);
+}
+
+
+let form = documents.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
