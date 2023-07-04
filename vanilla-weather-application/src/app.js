cc719fa0bd5ab9fb12416f2271bb5a0451d130c7
vcadalso <vanessa.cadalso@gmail.com>
2023-07-04 09:28:50 -0400

    Fixed APIurl mistake

--- vanilla-weather-application/src/app.js
+++ vanilla-weather-application/src/app.js
@@ -62,7 +62,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
     console.log(coordinates);
     let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 }
 
